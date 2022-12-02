9ab3e18a3a4312824b26c17f6b1ee8463b0a5d90
code01012019 <82946636+code01012019@users.noreply.github.com>
2022-12-02 20:46:51 +0100

    up

--- test.php
+++ test.php
@@ -7,16 +7,24 @@
 
 
 <?php
+echo date('h:i:s') . "<br>";
+
+//sleep for 3 seconds
+sleep(3);
 $x = 1;
 while($x <= 5) {
   echo "The number is: $x <br>";
   $x=$x+1;
+  sleep(1);
 }
 
+/*
 for ($x = 0; $x <= 10; $x++) {
     echo "The number is: $x <br>";
 } 
-?> 
+*/
+?>
+ 
 
 </body>
 </html>

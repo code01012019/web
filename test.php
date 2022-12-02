c4b7d0a2574b03f68b880d8152edd8d30d4781eb
code01012019 <82946636+code01012019@users.noreply.github.com>
2022-12-02 20:33:48 +0100

    up

--- test.php
+++ test.php
@@ -1,10 +1,8 @@
-
-
 <!DOCTYPE html>
 <html>
 <body>
 
-<h1>PHP test page</h1>
+<h1>PHP page</h1>
 
 <?php
 $x = 1;
@@ -12,7 +10,7 @@ while($x <= 5) {
   echo "The number is: $x <br>";
   $x=$x+1;
 }
-?>
+?> 
 
 </body>
 </html>
